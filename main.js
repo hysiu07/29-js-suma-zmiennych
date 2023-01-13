8ab90366d428d1460bd4e2f55456ca1dc801a2ef
Daniel <hysiu07@gmail.com>
2023-01-13 19:26:39 +0100

    task is finish

---
+++ main.js
@@ -0,0 +1,23 @@
+// mozna tak:
+
+let firstNumber = 5
+let secondNumber = 7
+
+function add (x, y){
+let sum = x + y
+return sum
+}
+
+console.log(add(firstNumber, secondNumber));
+
+
+// ale mozna i tak:
+
+
+let firstNumber1 = 5
+let secondNumber1 = 7
+
+function add1 (x, y){
+    return x + y
+}
+console.log(add(firstNumber1, secondNumber1));
